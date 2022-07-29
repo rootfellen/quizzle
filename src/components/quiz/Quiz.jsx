5c885d611cb83a70cce9fb1a7213c0f7514739dc
serhii <rootfellenua@gmail.com>
2022-07-29 14:50:48 -0400

    adding answer to quiz question

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -2,39 +2,31 @@ import React from "react";
 import "./Quiz.css";
 
 const Quiz = (props) => {
-  console.log(props);
+  const htmlDecode = (input) => {
+    const doc = new DOMParser().parseFromString(input, "text/html");
+    return doc.documentElement.textContent;
+  };
 
-  function unEscape(htmlStr) {
-    htmlStr = htmlStr.replace(/&lt;/g, "<");
-    htmlStr = htmlStr.replace(/&gt;/g, ">");
-    htmlStr = htmlStr.replace(/&quot;/g, '"');
-    htmlStr = htmlStr.replace(/&#39;/g, "'");
-    htmlStr = htmlStr.replace(/&amp;/g, "&");
-    return htmlStr;
-  }
-
-  const questions = props.data.results.map((q, idx) => (
-    <div key={idx} className="quiz-question">
-      <h3 className="question-title">{q.question}</h3>
-      <ul className="quiz-answers"></ul>
-      <hr className="separator" />
-    </div>
-  ));
+  const questions = props.data.results.map((q, idx) => {
+    return (
+      <div key={idx} className="quiz-question">
+        <h3 className="question-title">{htmlDecode(q.question)}</h3>
+        <div className="quiz-answers">
+          {q.incorrect_answers.map((a, idx) => (
+            <div>
+              <input key={idx} type="radio" name={a} value={a} />
+              <label htmlFor={a}>{a}</label>
+            </div>
+          ))}
+        </div>
+        <hr className="separator" />
+      </div>
+    );
+  });
   return (
     <>
       <div className="quiz-container">
-        <div className="quiz-wrapper">
-          {/* <div className="quiz-question">
-            <h3 className="question-title">Question title</h3>
-            <ul className="quiz-answers">
-              <li>Answer 1</li>
-              <li>Answer 2</li>
-              <li>Answer 3</li>
-              <li>Answer 4</li>
-            </ul>
-          </div> */}
-          {questions}
-        </div>
+        <div className="quiz-wrapper">{questions}</div>
         <div className="quiz-result">
           <button>Check result</button>
         </div>
