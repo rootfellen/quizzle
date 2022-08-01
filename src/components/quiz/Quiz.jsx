8bb0d179d4cbfcf9e429c82b936b56bab0668894
serhii <rootfellenua@gmail.com>
2022-08-01 19:48:48 -0400

    setting state for quiz component

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -1,7 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Quiz.css";
+import { nanoid } from "nanoid";
 
 const Quiz = (props) => {
+  const [questions, setQuestions] = useState([]);
+  const [answers, setAnswers] = useState([]);
   // ==== UNESCAPING HTML DATA FROM API ==== //
 
   const htmlDecode = (input) => {
@@ -9,38 +12,65 @@ const Quiz = (props) => {
     return doc.documentElement.textContent;
   };
 
-  // ==== CREATING LIST OF QUESTIONS WITH ANSWERS ==== //
+  useEffect(() => {
+    setQuestions(() => {
+      return props.data.map((q) => {
+        const incorrectAnswers = q.incorrect_answers;
+        const correctAnswer = q.correct_answer;
+        const arr = [...incorrectAnswers];
+        arr.splice(
+          Math.floor(Math.random() * (incorrectAnswers.length + 1)),
+          0,
+          correctAnswer
+        );
+        return {
+          question: q.question,
+          answers: arr,
+          id: nanoid(),
+        };
+      });
+    });
+  }, []);
 
-  // const options = incorrectAnswers.map((a) => a.splice(0, 0, correctAnswers));
-  const questions = props.data.map((q, idx) => {
-    let arr = [];
-    const incorrectAnswers = q.incorrect_answers;
-    const correctAnswer = q.correct_answer;
-    arr.push(...incorrectAnswers);
-    arr.splice(
-      Math.floor(Math.random() * (incorrectAnswers.length + 1)),
-      0,
-      correctAnswer
-    );
-    return (
-      <div key={idx} className="quiz-question">
-        {htmlDecode(q.question)}
-        <div className="quiz-answers">
-          {arr.map((a, idx) => {
-            return (
-              <div className="quiz-answer" key={idx}>
-                {htmlDecode(a)}
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    );
-  });
+  console.log(questions);
+  // ==== CREATING LIST OF QUESTIONS WITH ANSWERS (INCLUDING CORRECT ONE) ==== //
+
+  // const questions = props.data.map((q, idx) => {
+  //   let arr = [];
+  //   const incorrectAnswers = q.incorrect_answers;
+  //   const correctAnswer = q.correct_answer;
+  //   arr.push(...incorrectAnswers);
+  //   arr.splice(
+  //     Math.floor(Math.random() * (incorrectAnswers.length + 1)),
+  //     0,
+  //     correctAnswer
+  //   );
+
+  //   return (
+  //     <div key={idx} className="quiz-question">
+  //       {htmlDecode(q.question)}
+  //       <div className="quiz-answers">
+  //         {arr.map((a, idx) => {
+  //           return (
+  //             <div className="quiz-answer" key={idx}>
+  //               <input
+  //                 className="quiz-answer-choice"
+  //                 type="radio"
+  //                 name="answer"
+  //                 value={htmlDecode(a)}
+  //               />
+  //               <label htmlFor="answer">{htmlDecode(a)}</label>
+  //             </div>
+  //           );
+  //         })}
+  //       </div>
+  //     </div>
+  //   );
+  // });
   return (
     <>
       <div className="quiz-container">
-        <div className="quiz-wrapper">{questions}</div>
+        <div className="quiz-wrapper"></div>
         <div className="quiz-result">
           <button>Check result</button>
         </div>
