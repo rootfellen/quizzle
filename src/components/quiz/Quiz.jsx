8a9ba1ebdf7adf7be9fa1c8d8d968ec3f04cdaac
serhii <rootfellenua@gmail.com>
2022-08-04 10:36:52 -0400

    radio input for each question fix

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -11,6 +11,12 @@ const Quiz = (props) => {
     return doc.documentElement.textContent;
   };
 
+  const checkAnswer = (e) => {
+    const output = questions.map((q) => q.question.id);
+    console.log(output);
+    console.log(e.target.id);
+  };
+  console.log(questions);
   useEffect(() => {
     setQuestions(() => {
       return props.data.map((q) => {
@@ -28,8 +34,12 @@ const Quiz = (props) => {
             title: htmlDecode(q.question),
             correctAnswer: correctAnswer,
             incorrectAnswers: {
-              answers: [...newArr],
-              answerId: [...newArr].map((a) => nanoid(a)),
+              answers: [...newArr].map((a) => {
+                return {
+                  answer: a,
+                  id: nanoid(),
+                };
+              }),
             },
             id: nanoid(),
           },
@@ -39,55 +49,27 @@ const Quiz = (props) => {
     });
   }, []);
 
-  console.log(questions);
   // ==== CREATING LIST OF QUESTIONS WITH ANSWERS (INCLUDING CORRECT ONE) ==== //
-
-  // const questions = props.data.map((q, idx) => {
-  //   let arr = [];
-  //   const incorrectAnswers = q.incorrect_answers;
-  //   const correctAnswer = q.correct_answer;
-  //   arr.push(...incorrectAnswers);
-  //   arr.splice(
-  //     Math.floor(Math.random() * (incorrectAnswers.length + 1)),
-  //     0,
-  //     correctAnswer
-  //   );
-
-  //   return (
-  //     <div key={idx} className="quiz-question">
-  //       {htmlDecode(q.question)}
-  //       <div className="quiz-answers">
-  //         {arr.map((a, idx) => {
-  //           return (
-  //             <div className="quiz-answer" key={idx}>
-  //               <input
-  //                 className="quiz-answer-choice"
-  //                 type="radio"
-  //                 name="answer"
-  //                 value={htmlDecode(a)}
-  //               />
-  //               <label htmlFor="answer">{htmlDecode(a)}</label>
-  //             </div>
-  //           );
-  //         })}
-  //       </div>
-  //     </div>
-  //   );
-  // });
   return (
     <>
       <div className="quiz-container">
-        <div className="quiz-wrapper">
+        <form className="quiz-wrapper">
           {questions.map((q) => {
             return (
               <div key={q.question.id} className="quiz-question">
                 <h3 className="questions-title">{q.question.title}</h3>
                 <div className="quiz-answers">
-                  {q.question.incorrectAnswers.answers.map((a) => {
+                  {q.question.incorrectAnswers.answers.map((a, idx) => {
                     return (
-                      <div key={nanoid()} className="quiz-answer-wrapper">
-                        <input type="radio" name={a} id={nanoid()} value={a} />
-                        <label htmlFor={a}>{htmlDecode(a)}</label>
+                      <div key={a.id} className="quiz-answer">
+                        <input
+                          type="radio"
+                          id={a.id}
+                          name={q.question.id}
+                          value={a.answer}
+                          onClick={checkAnswer}
+                        />
+                        <label htmlFor={a.id}>{htmlDecode(a.answer)}</label>
                       </div>
                     );
                   })}
@@ -95,7 +77,7 @@ const Quiz = (props) => {
               </div>
             );
           })}
-        </div>
+        </form>
         <div className="quiz-result">
           <button>Check result</button>
         </div>
